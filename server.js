8d8f5fd094ec7cef3e755ddcd8fdee04ecebd9cf
Claus Bové <clbo@kea.dk>
2015-08-26 21:36:00 +0200

    created a var server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
 
 	res.writeHead(200, {'Content-Type' : 'text/plain'});
 	res.end("Hello World\n");
 	
-}).listen(process.env.PORT || 3000);
+});
+
+server.listen(process.env.PORT || 3000);
