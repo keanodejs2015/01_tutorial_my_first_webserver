377f1062afcb70f903861f2ef60bd893a6e82a34
Claus Bové <clbo@kea.dk>
2015-09-27 23:51:34 +0200

    error in server.js missing ;

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var http = require('http');
 
-var server = http.createServer(function (req, res) {
+var serve = http.createServer(function (req, res) {
 
 	res.writeHead(200, {'Content-Type' : 'text/plain'});
-	res.end("Hello World\n")
+	res.end("Hello World\n");
 	console.log('A request was made / ' + req.method);
 });
 
