6675ca02fc4e848d6d7d20a99e395db8c9ea1565
Claus Bové <clbo@kea.dk>
2015-09-27 23:33:44 +0200

    error in server.js added for testing travis

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var server = http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type' : 'text/plain'});
 	res.end("Hello World\n");
 	console.log('A request was made / ' + req.method);
-	
+	---
 });
 
 server.listen(process.env.PORT || 3000);
