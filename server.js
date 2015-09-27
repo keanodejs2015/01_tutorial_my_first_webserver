c9b69985bb50ff5e7083d8a29e635fa827f1b83b
Claus Bové <clbo@kea.dk>
2015-09-27 23:52:43 +0200

    error in server.js fixed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-var serve = http.createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
 
 	res.writeHead(200, {'Content-Type' : 'text/plain'});
 	res.end("Hello World\n");
