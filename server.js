92be7cbb7b37f82536fa9738a2938ba52598025d
Claus Bové <clbo@kea.dk>
2015-09-27 12:36:10 +0200

    yaris.yml file added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var server = http.createServer(function (req, res) {
 
 	res.writeHead(200, {'Content-Type' : 'text/plain'});
 	res.end("Hello World\n");
+	console.log('A request was made / ' + req.method);
 	
 });
 
